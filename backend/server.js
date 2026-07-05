e6bb3d4abd2341f58dace68ab37c5c239d26e00e
codersuman <sm093615@gmail.com>
2026-07-05 20:46:59 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+// import express from "express"; // not for module in script in package.json
+
+
+const express = require("express");
+const app = express();
+
+app.get("/api/notes",(req,res)=>{
+    res.send("You got 5 notes");
+});
+
+app.listen(5001,()=>{
+    console.log("Server started on PORT: 5001");
+});
