7a0228e8961ad27df0ebc72bf934484489532fdc
codersuman <sm093615@gmail.com>
2026-07-13 00:46:09 +0530

    post put delete.

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,16 @@ app.get("/api/notes",(req,res)=>{
     res.status(200).send("You got 5 notes ");
 });
 
-app.post("/api/notes",(req,res)=>{
-    res.status(201).send("Your notes created successfully");
+app.post("api/notes",(req,res)=>{
+    res.status(201).json({message:"Note created successfully!"})
+})
+app.put("api/notes/:id",(req,res)=>{
+    res.status(200).json({message:"Note updated successfully!"})
 })
 
+app.delete("api/notes/:id",(req,res)=>{
+    res.status(200).json({message:"Note updated successfully!"})
+})
 
 
 app.listen(5001,()=>{
