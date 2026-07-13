6e4356c94a6e46823c443b09f0e14ff80d0eb4e7
codersuman <sm093615@gmail.com>
2026-07-14 01:03:12 +0530

    mongo connections

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,19 @@
-import mongoose from "mongoose"
+/** @format */
 
-export const connectDB = async()=>{
-    try{
-       await mongoose.connect("mongodb+srv://codersuman01_db_user:yrqoQLMV3H8bLbG9@cluster0.jmv4vzs.mongodb.net/?appName=Cluster0");
-            console.log("MONGODB CONNECTED SUCCESFULLY")
-    } catch(error){
-        console.error("ERROR CONNECTING TO MONGODB",error);
-        process.exit(1); // exit with failure
+import mongoose from "mongoose";
 
+export const connectDB = async () => {
+  try {
+    const mongoUri = process.env.MONGO_URI;
+
+    if (!mongoUri) {
+      throw new Error("MONGO_URI is not defined in environment variables");
     }
-}
+
+    await mongoose.connect(mongoUri);
+    console.log("MONGODB CONNECTED SUCCESFULLY");
+  } catch (error) {
+    console.error("ERROR CONNECTING TO MONGODB", error);
+    process.exit(1); // exit with failure
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // const express = require("express");
 const app = express();
@@ -12,10 +15,6 @@ const PORT = process.env.PORT || 5001;
 
 app.use("/api/notes", notesRoutes);
 
-
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
 });
-
-
-// mongodb+srv://codersuman01_db_user:yrqoQLMV3H8bLbG9@cluster0.jmv4vzs.mongodb.net/?appName=Cluster0
