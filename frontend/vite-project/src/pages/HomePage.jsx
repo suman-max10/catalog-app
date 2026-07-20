f2015d71e5d06b9ed27c4d7037ef68befb8b3483
codersuman <sm093615@gmail.com>
2026-07-20 11:28:28 +0530

    Rate limit completetd.

--- frontend/vite-project/src/pages/HomePage.jsx
+++ frontend/vite-project/src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/NavBar";
+import RateLimitedUI from "../components/RateLimitedUI";
 const HomePage = () => {
+
+  const [isRateLimited, setIsRateLimited] = useState(true);
   return (
     <div className='min-h-screen'>
       <NavBar />
+      {isRateLimited && <RateLimitedUI />}
     </div>
   );
 };
