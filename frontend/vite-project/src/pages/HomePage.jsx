43240701608c1bdcc7c3177b011e17ed77058ed5
codersuman <sm093615@gmail.com>
2026-07-22 10:08:20 +0530

    note card start.

--- frontend/vite-project/src/pages/HomePage.jsx
+++ frontend/vite-project/src/pages/HomePage.jsx
@@ -34,6 +34,19 @@ const HomePage = () => {
     <div className='min-h-screen'>
       <NavBar />
       {isRateLimited && <RateLimitedUI />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading Notes...</div>}
+        
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note)=>(
+             
+             <NoteCard key={note._id} note={note} />
+
+        ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
