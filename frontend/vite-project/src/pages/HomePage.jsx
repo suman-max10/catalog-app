9755b905dc6c957e3c85662a22a599cf0a4b4c3f
codersuman <sm093615@gmail.com>
2026-07-22 08:29:17 +0530

    home page.

--- frontend/vite-project/src/pages/HomePage.jsx
+++ frontend/vite-project/src/pages/HomePage.jsx
@@ -1,11 +1,29 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios"
 const HomePage = () => {
 
   const [isRateLimited, setIsRateLimited] = useState(true);
+  const[notes,setnotes] = useState([])
+  const[loading,setLoading] = useState(true);
+ 
+  useEffect(() => {
+    const fetchNotes =  async() => {
+    try{
+      const res = await axios.post("http://localhost:5001/api/notes")
+     
+      console.log(res.data);
+
+    }catch(error){
+      console.log("Error featching notes");
+    }
+    };
+    fetchNotes();
+  },[])
+
   return (
     <div className='min-h-screen'>
       <NavBar />
