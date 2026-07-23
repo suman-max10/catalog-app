516a141437e2f31fa50220bd536cb3cd182bcccb
codersuman <sm093615@gmail.com>
2026-07-23 09:26:50 +0530

    CREATE PAGE MAKING.

--- frontend/vite-project/src/pages/CreatePage.jsx
+++ frontend/vite-project/src/pages/CreatePage.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+/** @format */
+
+import { ArrowLeftIcon } from "lucide-react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const CreatePage = () => {
-  return (
-    <div>CreatePage</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false);
 
-export default CreatePage
+  const handleSubmit = () => {};
+
+  return (
+    <div className='min-h-screen bg-base-200'>
+      <div className='container mx-auto px-4 py-8'>
+        <div className='max-w-2xl mx-auto'>
+          <Link to={"/"} className='btn btn-ghost mb-6'>
+            <ArrowLeftIcon className='size-5' />
+            Back To Notes
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
